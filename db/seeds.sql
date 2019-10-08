c8e5304abaa84f6b330924ab7ecb55062abc1ad8
Carmen SanDiego <efemar82@gmail.com>
2019-10-07 20:52:36 -0700

    created sql files and set up config files

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('Guacamole Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Gardenburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quadruple Bypass Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Original Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Company Burger');
