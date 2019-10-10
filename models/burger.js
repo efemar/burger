cb4d7b30f59c01ff834b64c119f0c780083fadef
Carmen SanDiego <efemar82@gmail.com>
2019-10-09 22:00:43 -0700

    models file

--- models/burger.js
+++ models/burger.js
@@ -3,3 +3,35 @@
 //create the code that will call the ORM functions using burger specific input for the ORM.
 
 // * Export at the end of the `burger.js` file.
+
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+
+var orm = require("../config/orm");
+
+
+var cat = {
+    all: function(cb) {
+      orm.all("burgers", function(res) {
+        cb(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    create: function(cols, vals, cb) {
+      orm.create("burgers", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    update: function(objColVals, condition, cb) {
+      orm.update("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    },
+    delete: function(condition, cb) {
+      orm.delete("burgers", condition, function(res) {
+        cb(res);
+      });
+    }
+  };
+  
+  // Export the database functions for the controller (catsController.js).
+  module.exports = burger;
